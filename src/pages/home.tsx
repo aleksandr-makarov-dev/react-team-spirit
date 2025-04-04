57752f115b45b659c6a3bd2c0017f951dca6451c
Aleksandr <ac5295@student.jamk.fi>
2025-04-05 01:48:31 +0300

    feat(general): add dependencies, add styles to button and input components

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,22 +3,25 @@ import { Input } from "@/components/ui/input";
 
 export function HomePage() {
   return (
-    <div className="p-8 space-y-3">
-      <div className="flex gap-2">
-        <Input placeholder="Search..." invalid />
-        <Button onClick={() => console.log("clicked")}>Search</Button>
+    <>
+      <title>Home</title>
+      <div className="p-8 space-y-3">
+        <div className="flex gap-2">
+          <Input placeholder="Search..." invalid />
+          <Button onClick={() => console.log("clicked")}>Search</Button>
+        </div>
+        <div className="flex gap-2">
+          <Input placeholder="Search..." />
+          <Button onClick={() => console.log("clicked")}>Search</Button>
+        </div>
+        <div className="flex flex-col space-y-3">
+          <Button variant="default">Button</Button>
+          <Button variant="secondary">Button</Button>
+          <Button variant="outline">Button</Button>
+          <Button variant="soft">Button</Button>
+          <Button loading>Button</Button>
+        </div>
       </div>
-      <div className="flex gap-2">
-        <Input placeholder="Search..." />
-        <Button onClick={() => console.log("clicked")}>Search</Button>
-      </div>
-      <div className="flex flex-col space-y-3">
-        <Button variant="default">Button</Button>
-        <Button variant="secondary">Button</Button>
-        <Button variant="outline">Button</Button>
-        <Button variant="soft">Button</Button>
-        <Button loading>Button</Button>
-      </div>
-    </div>
+    </>
   );
 }
